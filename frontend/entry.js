1d94d3f50d7be647f1690dff551a323cf5dcb2ba
Matthias Jenny <matthiascjenny@gmail.com>
2018-08-04 15:02:26 -0400

    Refactor view

--- frontend/entry.js
+++ frontend/entry.js
@@ -1,45 +1,29 @@
-import startAsync from './click_handlers/async.js';
-import startSync from './click_handlers/sync.js';
-import appendCode from './utils/append_code.js';
-import { hyphenize } from './utils/convert_string.js';
+import render from './utils/render.js';
 
 document.addEventListener('DOMContentLoaded', function() {
-  window.syncTypes.concat(window.asyncTypes).forEach(function(type) {
-    const source = document.getElementById(`${hyphenize(type)}-source`);
-    appendCode(type, source);
-  });
+  render();
 
-  window.syncTypes.forEach(function(type) {
-    const form = document.getElementById(`${hyphenize(type)}-form`);
-    form.onsubmit = startSync(type);
-  });
+  // const inputIds = window.syncTypes
+  //   .concat(window.asyncTypes)
+  //   .map(function(type) {
+  //     return `${hyphenize(type)}-n`;
+  //   });
 
-  window.asyncTypes.forEach(function(type) {
-    const form = document.getElementById(`${hyphenize(type)}-form`);
-    form.onsubmit = startAsync(type);
-  });
+  // function nToSubmit(idString) {
+  //   return `${idString.slice(0, idString.length - 2)}-submit`;
+  // }
 
-  const inputIds = window.syncTypes
-    .concat(window.asyncTypes)
-    .map(function(type) {
-      return `${hyphenize(type)}-n`;
-    });
-
-  function nToSubmit(idString) {
-    return `${idString.slice(0, idString.length - 2)}-submit`;
-  }
-
-  const numberInputs = inputIds.map(function(id) {
-    return document.getElementById(id);
-  });
-  numberInputs.forEach(function(input) {
-    input.onchange = function() {
-      const submitButton = document.getElementById(nToSubmit(input.id));
-      if (parseInt(input.value) > 0) {
-        submitButton.disabled = false;
-      } else {
-        submitButton.disabled = true;
-      }
-    };
-  });
+  // const numberInputs = inputIds.map(function(id) {
+  //   return document.getElementById(id);
+  // });
+  // numberInputs.forEach(function(input) {
+  // input.onchange = function() {
+  //   const submitButton = document.getElementById(nToSubmit(input.id));
+  //   if (parseInt(input.value) > 0) {
+  //     submitButton.disabled = false;
+  //   } else {
+  //     submitButton.disabled = true;
+  //   }
+  // };
+  // });
 });
---
+++ frontend/utils/render.js
@@ -0,0 +1,83 @@
+import startAsync from '../click_handlers/async.js';
+import startSync from '../click_handlers/sync.js';
+import appendCode from './append_code.js';
+import { hyphenize } from './convert_string.js';
+
+export default function() {
+  const main = document.getElementById('main');
+
+  const types = [
+    'sync',
+    'async',
+    'syncBusy',
+    'asyncBusy',
+    'syncMemo',
+    'asyncMemo',
+  ];
+
+  types.forEach(function(type) {
+    const hyphenizedType = hyphenize(type);
+    const container = document.createElement('div');
+    container.classList.add('container');
+    container.id = `${hyphenizedType}-container`;
+
+    const h2 = document.createElement('h2');
+    h2.innerText = `${type}Fib`;
+    container.append(h2);
+
+    const source = document.createElement('div');
+    source.classList.add('source');
+    source.id = `${hyphenizedType}-source`;
+    appendCode(type, source);
+    container.append(source);
+
+    const h3 = document.createElement('h3');
+    h3.innerText = 'Benchmark';
+    container.append(h3);
+
+    const form = document.createElement('form');
+    form.id = `${type}-form`;
+    form.onsubmit = type[0] === 's' ? startSync(type) : startAsync(type);
+
+    const numberInput = document.createElement('input');
+    numberInput.id = `${hyphenizedType}-n`;
+    numberInput.type = 'number';
+    numberInput.placeholder = 'n';
+    form.append(numberInput);
+
+    const submitInput = document.createElement('input');
+    submitInput.classList.add('submit');
+    submitInput.id = `${hyphenizedType}-submit`;
+    submitInput.type = 'submit';
+    submitInput.disabled = true;
+    submitInput.value = 'Invoke';
+    form.append(submitInput);
+
+    numberInput.onchange = function() {
+      if (parseInt(numberInput.value) > 0) {
+        submitInput.disabled = false;
+      } else {
+        submitInput.disabled = true;
+      }
+    };
+
+    container.append(form);
+
+    const resultContainer = document.createElement('div');
+    resultContainer.classList.add('result-container');
+
+    const result = document.createElement('div');
+    result.classList.add('result');
+    result.id = `${hyphenizedType}-result`;
+    resultContainer.append(result);
+
+    const resultTime = document.createElement('div');
+    resultTime.classList.add('result');
+    resultTime.id = `${hyphenizedType}-time`;
+    resultContainer.append(resultTime);
+
+    container.append(resultContainer);
+
+    main.append(container);
+  });
+}
