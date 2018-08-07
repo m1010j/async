559379e9bb497ce09b3c205540f9635c49731105
Matthias Jenny <matthiascjenny@gmail.com>
2018-08-06 21:01:22 -0400

    Add commas when rendering result

--- frontend/utils/display_result.js
+++ frontend/utils/display_result.js
@@ -1,11 +1,16 @@
 import nth from './nth.js';
+import stringify from './stringify.js';
 
 export default function(n, duration, result, type) {
   document.getElementById(`${type}-submit`).disabled = false;
   const spinnerContainer = document.getElementById(`${type}-spinner-container`);
   spinnerContainer.classList.add('hidden');
+  let resultString = result.toString();
+  if (!(resultString[1] === '.')) {
+    resultString = stringify(result);
+  }
   document.getElementById(`${type}-result`).innerHTML =
-    `The ${nth(n)} ` + `Fibonacci number is ${result}.`;
+    `The ${nth(n)} ` + `Fibonacci number is ${resultString}.`;
   document.getElementById(`${type}-time`).innerHTML =
-    `It took ` + `${duration} milliseconds to calculate this.`;
+    `It took ` + `${stringify(duration)} milliseconds to calculate this.`;
 }
